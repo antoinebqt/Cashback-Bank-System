e0dba2bf38e655a34e53fb873fcf9fd7c4d0dbeb
MouradKarrakchou <91179433+MouradKarrakchou@users.noreply.github.com>
2023-10-10 11:58:58 +0200

    Setup Project DB

--- bank-service/src/main/java/fr/teama/bankservice/repository/BankDataRepository.java
+++ bank-service/src/main/java/fr/teama/bankservice/repository/BankDataRepository.java
@@ -1,7 +1,8 @@
 package fr.teama.bankservice.repository;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-public interface BankDataRepository extends JpaRepository<RocketData, Long> {
+public interface BankDataRepository extends JpaRepository<String, Long> {
 }
--- cashback-service/src/main/java/fr/teama/cashbackservice/repository/CashbackDataRepository.java
+++ cashback-service/src/main/java/fr/teama/cashbackservice/repository/CashbackDataRepository.java
@@ -1,7 +1,8 @@
 package fr.teama.cashbackservice.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CashbackDataRepository extends JpaRepository<RocketData, Long> {
+public interface CashbackDataRepository extends JpaRepository<String, Long> {
 }
