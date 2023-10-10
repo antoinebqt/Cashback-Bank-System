70165ad700c04a759540c84ad7cac1f94f710155
Ayoub <youbima17@gmail.com>
2023-10-10 11:23:22 +0200

    initialization

---
+++ bank-service/src/test/java/fr/teama/bankservice/BankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.teama.bankservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ cashback-service/src/test/java/fr/teama/cashbackservice/CashbackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.teama.cashbackservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashbackServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
