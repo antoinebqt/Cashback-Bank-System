09d41c44abae9e41882345a47592c15fa0b44b67
MouradKarrakchou <91179433+MouradKarrakchou@users.noreply.github.com>
2023-10-20 14:45:37 +0200

    Creation of a Payment #4

--- bank-service/src/main/java/fr/teama/bankservice/repository/TransactionRepository.java
+++ bank-service/src/main/java/fr/teama/bankservice/repository/TransactionRepository.java
@@ -1,4 +1,10 @@
 package fr.teama.bankservice.repository;
 
-public interface TransactionRepository {
+import fr.teama.bankservice.models.Card;
+import fr.teama.bankservice.models.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
