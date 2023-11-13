069d23f5eeeadfd0beb5a2cf14aaac65a535faa4
Ayoub <youbima17@gmail.com>
2023-11-13 16:27:27 +0100

    store logic #6

--- external-decathlon-mock-service/src/main/java/fr/teama/externaldecathlonmockservice/components/PurchaseReturnedManager.java
+++ external-decathlon-mock-service/src/main/java/fr/teama/externaldecathlonmockservice/components/PurchaseReturnedManager.java
@@ -1,5 +1,6 @@
 package fr.teama.externaldecathlonmockservice.components;
 
+import fr.teama.externaldecathlonmockservice.helpers.LoggerHelper;
 import fr.teama.externaldecathlonmockservice.interfaces.PurchaseReturnedRetriever;
 import org.springframework.stereotype.Component;
 
@@ -15,11 +16,20 @@ public class PurchaseReturnedManager implements PurchaseReturnedRetriever {
     @Override
     public List<Long> getPurchasedReturned(List<Long> purchasedItems) {
         List<Long> purchaseReturned = new ArrayList<>();
+        LoggerHelper.logInfo("ID: " + purchaseReturnedList.get(0));
+        LoggerHelper.logInfo("ID: " + purchaseReturnedList.get(1));
+        LoggerHelper.logInfo("ID: " + purchaseReturnedList.get(2));
+        LoggerHelper.logInfo("ID: " + purchaseReturnedList.get(3));
+        LoggerHelper.logInfo("ID: " + purchaseReturnedList.get(4));
         for(Long id : purchasedItems) {
+            LoggerHelper.logInfo("ID: " + id);
             if(purchaseReturnedList.contains(id)) {
+                LoggerHelper.logInfo("IN IF ");
                 purchaseReturned.add(id);
             }
         }
+
+        LoggerHelper.logInfo("LIST: " + purchaseReturned.toString());
         return purchaseReturned;
     }
 
