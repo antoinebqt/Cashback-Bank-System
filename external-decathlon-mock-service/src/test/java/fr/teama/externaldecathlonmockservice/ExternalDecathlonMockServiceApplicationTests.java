c9c94f41614dd8a5a3c7adcf20661c2452838543
Ayoub <youbima17@gmail.com>
2023-11-13 12:45:34 +0100

    store communication #6

---
+++ external-decathlon-mock-service/src/test/java/fr/teama/externaldecathlonmockservice/ExternalDecathlonMockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.teama.externaldecathlonmockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalDecathlonMockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
